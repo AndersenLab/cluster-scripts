4cd6de62a01dbd4fd2783d56167e9a753315250b
Mostafa Zamanian <mzamanian@lg-1r14-n04.guillimin.clumeq.ca>
2015-02-05 18:11:54 -0500

    freebayes_joint

--- freebayes_joint.sh
+++ freebayes_joint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#PBS -l nodes=1:ppn=12
-#PBS -l walltime=6:00:00
+#PBS -l nodes=1:ppn=16
+#PBS -l walltime=36:00:00
 #PBS -A hpt-060-aa
 #PBS -V
 #PBS -N freebayes
@@ -12,12 +12,12 @@
 
 cd $working_dir
 
-export OMP_NUM_THREADS=12
+export OMP_NUM_THREADS=16
 
 output_vcf=$vcf_dir/$sample.freebayes.vcf
 complete_vcf=$vcf_dir/$sample.freebayes.vcf.gz
 
-freebayes-parallel <(fasta_generate_regions.py $reference.fai 100000) $OMP_NUM_THREADS -f $reference -u -C 5 ${sample}.bam > $output_vcf
+freebayes-parallel <(fasta_generate_regions.py $reference.fai 100000) $OMP_NUM_THREADS -f $reference -u -C 5 $bam_list > $output_vcf
 
 # Add in contigs so file can be compressed, this is for WS245.
 cat $output_vcf | awk 'NR == 3 {print $0; print "##contig=<ID=I,length=15072434>\n##contig=<ID=II,length=15279421>\n##contig=<ID=III,length=13783801>\n##contig=<ID=IV,length=17493829>\n##contig=<ID=V,length=20924180>\n##contig=<ID=X,length=17718942>\n##contig=<ID=MtDNA,length=13794>"} NR != 3 { print $0 }' | bcftools view -O z > $complete_vcf
