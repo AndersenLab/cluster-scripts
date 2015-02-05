092ba8e073380c3baef6fd1c2344bd367b838030
Mostafa Zamanian <mzamanian@lg-1r14-n04.guillimin.clumeq.ca>
2015-02-05 10:56:27 -0500

    adding bamsurgeon

---
+++ bamsurgeon.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#PBS -l nodes=1:ppn=12
+#PBS -l walltime=12:00:00
+#PBS -V
+#PBS -N bamsurgeon
+
+source setup.sh
+
+export OMP_NUM_THREADS=12
+
+varset=$varset_dir/varset${n}.bed
+bamfile=N2.bam
+outbam=N2_test_set_${n}.bam
+sorted_bam=${outbam/.bam/}.sorted.bam
+
+python $addsnv -v $varset -r $reference -f $bamfile -o $outbam -p $OMP_NUM_THREADS --aligner mem --samtofastq $samtofastq --tmpdir ${n} 
+
+samtools sort -@ $OMP_NUM_THREADS -T ${n}.sorting -O bam $outbam > $sorted_bam
+samtools index $sorted_bam
+
+if [[ -f $sorted_bam && -f $sorted_bam.bai ]];
+then
+   rm $outbam
+else
+   echo "File $sorted_bam or $sorted_bam.bai does not exist."
+fi
