241aa8bb7aefc4f3518220f63de84e69ac63dd9c
Mostafa Zamanian <mzamanian@lg-1r17-n03.guillimin.clumeq.ca>
2015-03-02 11:20:38 -0500

    telseq

---
+++ telseq.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#PBS -l nodes=1:ppn=1
+#PBS -l walltime=30:00:00
+#PBS -A hpt-060-aa
+#PBS -V
+#PBS -N telseq
+
+cd /gs/scratch/mzamanian/NU
+
+export OMP_NUM_THREADS=1
+export PATH=$PATH:/sb/project/hpt-060-aa/telseq/src/Telseq
+
+#telseq -z 'TTAGGC' $file.bam -o $file.telseq_elegans.TTAGGC.txt
+
+#telseq -z 'GTATGC' $file.bam -o $file.telseq.GTATGC.txt
+#telseq -z 'GTCTAG' $file.bam -o $file.telseq.GTCTAG.txt
+
+#telseq -z 'AGTCGT' $file.bam -o $file.telseq.AGTCGT.txt
+#telseq -z 'GATCGT' $file.bam -o $file.telseq.GATCGT.txt
+
+#telseq -z 'CTATGG' $file.bam -o $file.telseq.CTATGG.txt
+#telseq -z 'GCCATT' $file.bam -o $file.telseq.GCCATT.txt
+
+#telseq -z 'TCGGAT' $file.bam -o $file.telseq.TCGGAT.txt
+#telseq -z 'TGTGAC' $file.bam -o $file.telseq.TGTGAC.txt
+
+#telseq -z 'TTAGGC' -u $file.bam -o $file.telseq_elegans.TTAGGC.noreadgroup.txt
+telseq -z 'GTCTAG' -u $file.bam -o $file.telseq_elegans.GTCTAG.noreadgroup.txt
+
+
